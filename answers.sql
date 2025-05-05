62aa45624a7e743e6b329a55a29707057a0fbaea
mrmugo <627mrmugo@gmail.com>
2025-05-05 10:06:32 +0300

    Assignment Retake

--- answers.sql
+++ answers.sql
@@ -1,54 +1,39 @@
-< --Question 1 -- >
+
+ --  Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+-- Question 2
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
-
-       
-CREATE TABLE Products (
-    ProductID INT AUTO_INCREMENT PRIMARY KEY,
-    OrderID INT,
-    ProductName VARCHAR(100),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
-);
-
 INSERT INTO Orders (OrderID, CustomerName)
-VALUES 
+VALUES
 (101, 'John Doe'),
 (102, 'Jane Smith'),
 (103, 'Emily Clark');
 
-INSERT INTO Products (OrderID, ProductName)
-VALUES 
-(101, 'Laptop'),
-(101, 'Mouse'),
-(102, 'Tablet'),
-(102, 'Keyboard'),
-(102, 'Mouse'),
-(103, 'Phone');
 
-<--Question 2 -- >
-CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(100)
-);
-
-CREATE TABLE OrderDetails (
+CREATE TABLE Product (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
-
-INSERT INTO Orders (OrderID, CustomerName)
-VALUES 
-(101, 'John Doe'),
-(102, 'Jane Smith'),
-(103, 'Emily Clark');
-
-INSERT INTO OrderDetails (OrderID, Product, Quantity)
-VALUES 
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
 (101, 'Laptop', 2),
 (101, 'Mouse', 1),
 (102, 'Tablet', 3),
